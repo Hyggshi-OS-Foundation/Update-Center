c64e0c98e0fc4f02117f696bc9e99ec57dbfb03f
HyggshiOSDeveloper <hyggshidev@gmail.com>
2026-08-14 15:57:42 +0700

    fĩ p10

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,10 @@ int main(int argc, char* argv[])
     QApplication app(argc, argv);
     app.setApplicationName("Update Center");
     app.setOrganizationName("Hyggshi OS");
+    app.setWindowIcon(QIcon(":/resources/updatecenter.svg"));
 
     MainWindow window;
+    window.setWindowIcon(QIcon(":/resources/updatecenter.svg"));
     window.show();
 
     return app.exec();
